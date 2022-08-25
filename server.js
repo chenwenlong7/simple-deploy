241b25d61c38fd609d356304a2270afef92b364a
chenwenlong7 <435421355@qq.com>
2022-08-26 00:18:31 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('node:http');
+const fs = require('node:fs');
+
+const html = fs.readFileSync('./index.html');
+
+const server = http.createServer((req, res) => {
+  res.end(html)
+})
+server.listen(3000, () => {
+  console.log('Listen 3000')
+})
